811dbd5e014fbbdff221a71cc1140fabfd80d090
Haseeb Khan <haseeb.khan@digital.justice.gov.uk>
2021-03-02 17:09:42 +0000

    Adjusted the script to handle command line variables, adjusted the courts so that it only contains ones with information in, and included docker information for when the tests are ready to be put on a server.

--- src/test/scala/simulations/ProbationRecordScreen.scala
+++ src/test/scala/simulations/ProbationRecordScreen.scala
@@ -6,15 +6,45 @@ import io.gatling.http.Predef._
 
 class ProbationRecordScreen extends Simulation {
 
+  private def getProperty(propertyName: String, defaultValue: String): String = {
+    {
+      {
+        Option(System.getenv(propertyName))
+          .orElse(Option(System.getProperty(propertyName)))
+          .getOrElse(defaultValue)
+      }
+    }
+
+    //Date for message.
+  }
+  //Set user as (message count) in this case set to 1.
+  def userCount: Int = getProperty("Users", "2").toInt
+
+  //def rampDuration: Int = getProperty("Ramp_Duration", "10").toInt
+  def env: String = getProperty("Env","preprod")
+
+  //Run once through set duration to 0.
+  def testDuration: Int = getProperty("Duration", "0").toInt
+
+  def thinkTime: Int = getProperty("ThinkTime", "0").toInt
+
+  before {
+    println(s"Running tests with ${userCount} users.")
+    println(s"Test duration is: ${testDuration} minutes.")
+    println(s"The enviroment that is being tested against is: ${env}.")
+    println(s"The test is running with pauses/thinktimes of ${thinkTime} seconds between requests.")
+    //println(s"The test is running using ${userUsed} user.")
+  }
+
   val httpProtocol = http
-    .baseUrl("https://prepare-a-case-preprod.apps.live-1.cloud-platform.service.justice.gov.uk")
+    .baseUrl("https://prepare-a-case-"+env+".apps.live-1.cloud-platform.service.justice.gov.uk")
     .inferHtmlResources()
     .acceptHeader("image/webp,*/*")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-GB,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0")
 
-  val authUrl = "https://sign-in-preprod.hmpps.service.justice.gov.uk/auth"
+  val authUrl = "https://sign-in-"+env+".hmpps.service.justice.gov.uk/auth"
 
   val scn = scenario("ProbationRecordScreen")
     .feed(users)
@@ -30,23 +60,23 @@ class ProbationRecordScreen extends Simulation {
       .get(authUrl + "/login")
       .headers(headers_0)
       .check(status.is(200)))
-    .pause(10)
+    .pause(thinkTime)
     .exec(http("LoginForm")
       .post(authUrl + "/login")
       .headers(headers_2)
       .formParam("username", "${ParamUsername}")
       .formParam("password", "${ParamPassword}")
       .check(status.is(200)))
-    .pause(10)
+    .pause(thinkTime)
     .exec(http("SelectCourt")
       .get("/select-court/${CourtCode}")
       .headers(headers_4))
-    .pause(10)
+    .pause(thinkTime)
     .exec(http("GetCasesofDate")
       .get("/${CourtCode}/cases/2021-02-08")
       .headers(headers_4)
       .check(css("title").is("Cases - Prepare a case for sentence")))
-    .pause(10)
+    .pause(thinkTime)
     .exec(http("PostCurrentStatus")
       .post("/${CourtCode}/cases/2021-02-08")
       .headers(headers_8)
@@ -56,13 +86,13 @@ class ProbationRecordScreen extends Simulation {
       println("CaseNo is: " + session("caseNo").as[String])
       session
     })
-    .pause(2)
+    .pause(thinkTime)
     .exec(http("GetSummary")
       .get("/${CourtCode}/case/${caseNo}/summary")
       .headers(headers_4)
       .check(css("title").is("Case summary - Prepare a case for sentence"))
       .check(status.is(200)))
-    .pause(6)
+    .pause(thinkTime)
     .exec(http("GetRecordScreen")
       .get("/${CourtCode}/case/${caseNo}/record")
       .headers(headers_4)
