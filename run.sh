f06396b3b9d9b9e4f5b7631e91456a5d3c436985
John Evans <john.evans@digital.justice.gov.uk>
2021-04-13 15:20:47 +0100

    👷‍♀️ PIC-1286: Configuration to allow running tests from CI

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 TIMESTAMP=$(date +"%Y-%m-%dT%H-%M-%S")
-OUTPUT_PATH=cpmg-gatling-performance-tests
+OUTPUT_PATH=prepare-a-case-gatling-performance-tests
 
-mvn gatling:test -DUsers=1 -DDuration=1 -DEnv=preprod -DThinkTime=3 -DDate=2021-03-19 -q
+mvn gatling:test -DUsers=${PERF_TEST_USERS} -DDuration=${PERF_TEST_DURATION}  -DEnv=preprod -DThinkTime=3 -DDate=$(date +"%Y-%m-%d") -q
 
 aws s3 cp /app/target/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/reports --recursive
