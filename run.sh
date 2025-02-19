a5dca2886e9b641766e8b516a255d09b24dd58e7
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2025-02-19 16:51:48 +0000

    Changing config back

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
+TIMESTAMP=$(date +"%Y-%m-%dT%H-%M-%S")
+OUTPUT_PATH=prepare-a-case-gatling-performance-tests
+
 ./gradlew gatlingRun --all -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Denv=preprod -Dusers=${PERF_TEST_USERS} -Dramp=${PERF_TEST_DURATION} -Dpause=2
+
+aws s3 cp /app/target/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/reports --recursive
+aws s3 cp /app/createOutputVariables.csv s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/createOutputVariables.csv
