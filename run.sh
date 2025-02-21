da86521a6031b29871abdad8e93f4612842f702e
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2025-02-21 10:44:46 +0000

    Config change

--- run.sh
+++ run.sh
@@ -6,4 +6,25 @@ OUTPUT_PATH=prepare-a-case-gatling-performance-tests
 
 ./gradlew gatlingRun --all -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Denv=preprod -Dusers=${PERF_TEST_USERS} -Dramp=${PERF_TEST_DURATION} -Dpause=2
 
+echo "USERNAME: " ${USERNAME}
+
+# Start from the top-level directory
+start_directory="/app/build/reports/gatling/"
+
+# Function to recursively echo contents of a directory
+function echo_contents() {
+  local directory=$1
+  for entry in "$directory"/*; {
+    if [ -d "$entry" ]; then
+      echo "Directory: $entry"
+      echo_contents "$entry"
+    else
+      echo "File: $entry"
+    }
+  }
+}
+
+# Start from the specified directory
+echo_contents "$start_directory"
+
 aws s3 cp /app/build/reports/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/reports --recursive
