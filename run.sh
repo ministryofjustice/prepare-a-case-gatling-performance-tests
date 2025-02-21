123a378f86691b1fa577a024107cb790b51dfd97
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2025-02-21 12:33:20 +0000

    Adding back user load config

--- run.sh
+++ run.sh
@@ -6,25 +6,4 @@ OUTPUT_PATH=prepare-a-case-gatling-performance-tests
 
 ./gradlew --debug --stacktrace gatlingRun --all -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Denv=preprod -Dusers=${PERF_TEST_USERS} -Dramp=${PERF_TEST_DURATION} -Dpause=2
 
-echo "USERNAME: " ${USERNAME}
-
-# Start from the top-level directory
-start_directory="app/build/reports/gatling"
-
-# Function to recursively echo contents of a directory
-echo_contents () {
-  local directory=$1
-  for entry in "$directory"/*; do
-    if [ -d "$entry" ]; then
-      echo "Directory: $entry"
-      echo_contents "$entry"
-    else
-      echo "File: $entry"
-    fi
-  done
-}
-
-# Start from the specified directory
-echo_contents "$start_directory"
-
 aws s3 cp /app/build/reports/gatling/ s3://${S3_BUCKET_NAME}/${OUTPUT_PATH}/${TIMESTAMP}/reports --recursive
